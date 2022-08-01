350c8bf97320ade43cf4d00a4b925fc323002a82
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2022-08-02 00:46:30 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = expr.match(/.{10}/g);
+    const morseArr = arr.map(elem => elem.replace(/10/g,'.').replace(/11/g,'-').replace(/0/g,''));
+
+    for (let i = 0; i < morseArr.length; i++) {
+        if (MORSE_TABLE[morseArr[i]]) {
+            morseArr[i] = MORSE_TABLE[morseArr[i]]
+        } else {
+            morseArr[i] = ' '
+        }
+    }
+    return morseArr.join('');
 }
 
 module.exports = {
